d3bd55c173cb4a399c412051b42189a184cae9d8
IGLEinvo <igorlevocko@gmail.com>
2023-03-13 16:46:38 +0200

    make pullrequest

--- src/ua/lviv/iot/algo/part1/lab1/Main.java
+++ src/ua/lviv/iot/algo/part1/lab1/Main.java
@@ -2,17 +2,16 @@ package ua.lviv.iot.algo.part1.lab1;
 //
 public class Main {
     public static void main(String[] args) {
-        PreciousStone stone1 = new PreciousStone("sapphire", 1, "blue", 3, 1000);
+        PreciousStone[] stones ={
+        new PreciousStone(),
+         new PreciousStone  ("sapphire", 1, "blue", 3, 1000),
 
-        PreciousStone stone2 = new PreciousStone("Emerald", 4, "Green", 3, 3000);
+         PreciousStone.getInstance(),
+         PreciousStone.getInstance()
+    };
 
-        PreciousStone stone3 = PreciousStone.getInstance();
-        PreciousStone stone4 = PreciousStone.getInstance();
-
-        PreciousStone[] stones = {stone1, stone2, stone3, stone4};
         for (PreciousStone stone : stones) {
             System.out.println(stone.toString());
         }
     }
 }
-
--- src/ua/lviv/iot/algo/part1/lab1/PreciousStone.java
+++ src/ua/lviv/iot/algo/part1/lab1/PreciousStone.java
@@ -4,6 +4,7 @@ package ua.lviv.iot.algo.part1.lab1;
 import lombok.*;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Setter
 @Getter
 @ToString
@@ -14,19 +15,6 @@ public class PreciousStone {
     private float clarity;
     private float pricePerCarat;
     private static PreciousStone instance;
-
-    public PreciousStone(String name, float carat, String color, float clarity, float pricePerCarat) {
-        this.name = name;
-        this.carat = carat;
-        this.color = color;
-        this.clarity = clarity;
-        this.pricePerCarat = pricePerCarat;
-    }
-    @Override
-    public String toString() {
-        return "Name: " + this.name + ", Carat: " + this.carat + ", Color: " + this.color +
-                ", Clarity: " + this.clarity + ", Price per carat: " + this.pricePerCarat;
-    }
 
     public float getTotalPrice(){
         return pricePerCarat * carat;
